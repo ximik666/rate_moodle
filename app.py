7b16c83395a101fd022f4c85cd66c64821e6f201
ximik666 <zaiaidar@yandex.ru>
2015-06-29 21:09:02 +0300

    test

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
 # We need to import request to access the details of the POST request
 # and render_template, to render our templates (form and response)
 # we'll use url_for to get some URLs for the app on the templates
+import sys, codecs,
+reload(sys)
+sys.setdefaultencoding('utf-8')
+import sqlite3 as sql
 from flask import Flask, render_template, request, url_for
 
 # Initialize the Flask application
 app = Flask(__name__)
+
+def select_account_holder(params=()):
+    con = sql.connect("rate.db")
+    cur = con.cursor()
+    if params == ():
+        cur.execute("select * from fio")
+    else:
+        string = "select"
+        for i in xrange(len(params) - 1):
+            string += "%s,"
+        string += "%s"
+        string += " from account_holder"
+
+        result = cur.execute(string)
+        con.close()
+        return result.fetchall()
 
 # Define a route for the default URL, which loads the form
 @app.route('/')
