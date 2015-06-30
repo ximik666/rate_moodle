db000efdeb6d547100d9636fcdc87dfc7e412300
ximik666 <zaiaidar@yandex.ru>
2015-06-30 15:23:14 +0300

    sds

--- app.py
+++ app.py
@@ -5,44 +5,45 @@ import sys, codecs
 reload(sys)
 sys.setdefaultencoding('utf-8')
 import sqlite3 as sql
-from flask import Flask, render_template, request, url_for
+from flask import Flask, json, render_template, request, url_for,jsonify
 
 # Initialize the Flask application
 app = Flask(__name__)
 
-def select_account_holder(params=()):
+def select_account_holder():
     con = sql.connect("rate.db")
     cur = con.cursor()
-    if params == ():
-        cur.execute("select * from fio")
-    else:
-        string = "select"
-        for i in xrange(len(params) - 1):
-            string += "%s,"
-        string += "%s"
-        string += " from account_holder"
-
-        result = cur.execute(string)
-        con.close()
-        return result.fetchall()
+    result = cur.execute("select * from rate")
+    print result.fetchall()
 
 # Define a route for the default URL, which loads the form
 @app.route('/')
 def form():
     return render_template('form_submit.html')
 
+@app.route('/star')
+def stars():
+    return render_template('stars.html')
+
 # Define a route for the action of the form, for example '/hello/'
 # We are also defining which type of requests this route is 
 # accepting: POST requests in this case
 @app.route('/rate/', methods=['POST'])
-def hello():
-    name=request.form['yourname']
-    email=request.form['youremail']
-    return render_template('form_action.html', name=name, email=email)
+def rate():
+    id_course=request.form['id_course']
+    id_user=request.form['id_user']
+    select_account_holder()
+    return render_template('form_action.html', id_course=id_course, id_user=id_user)
+
+@app.route('/post', methods = ['PUT','POST'])
+def post():
+    # Get the parsed contents of the form data
+    print request.get_json(force=True)
+    return jsonify(request.get_json(force=True))
 
 # Run the app :)
 if __name__ == '__main__':
   app.run( 
         host="0.0.0.0",
-        port=int("5000")
+        port=int("5001")
   )
